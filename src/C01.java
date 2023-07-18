05179e4931e488273c91f473c1b9882aca3434b9
Halime <hlmmonder@gmail.com>
2023-07-18 14:00:14 +0200

    ilk degisiklik

--- src/C01.java
+++ src/C01.java
@@ -2,5 +2,6 @@ public class C01 {
     public static void main(String[] args) {
         System.out.println("Hello-");
          System.out.println("Hello-");
+        System.out.println("hi");
     }
 }
